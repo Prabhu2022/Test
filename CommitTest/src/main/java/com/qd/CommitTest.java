f081dcf575ab42f8b77fb1cc181bf22ebfae6bc9
HP <HP@HP-PC>
2022-05-17 12:44:47 +0530

    First Commit

--- CommitTest/src/main/java/com/qd/CommitTest.java
+++ CommitTest/src/main/java/com/qd/CommitTest.java
@@ -4,6 +4,31 @@ public class CommitTest {
 	
 	
 
+private String uName;
+
+private String age;
+
+
+	public String getuName() {
+	return uName;
+}
+
+
+public void setuName(String uName) {
+	this.uName = uName;
+}
+
+
+public String getAge() {
+	return age;
+}
+
+
+public void setAge(String age) {
+	this.age = age;
+}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
